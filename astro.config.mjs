bdd8dd7a1b2961a5490583ffa3837d11a4cf85e0
EdwardVE <ervasalloe@unal.edu.co>
2025-10-10 19:20:31 -0500

    Initial version

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import svelte from '@astrojs/svelte';
+
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
+  //TODO:Hace que la renderizacion se haga en el servidor
+  output: 'server',
+
   vite: {
     plugins: [tailwindcss()],
       server: {
@@ -15,4 +22,5 @@ export default defineConfig({
   },
   },
 
+  integrations: [svelte(), react()],
 });
